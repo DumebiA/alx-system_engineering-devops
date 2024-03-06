9aa6c097160c59b1361688a084c1b962a35bf45f
root <anyamkpa.ifeanyi@gmail.com>
2024-03-06 09:06:34 -0800

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,12 @@
-# Fixing Apache returning a 500 error
+# 0-strace_is_your_friend.pp
 
-exec { 'fix error':
-  provider => 'shell',
-  command  => 'sed -i "s/SomeOffensiveCode/CorrectCode/g" /path/to/apache_config.conf'
+exec { 'fix_apache':
+  command     => 'sed -i "s/SomeOffensiveCode/CorrectCode/g" /path/to/apache_config.conf',
+  refreshonly => true,
+}
+
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => Exec['fix_apache'],
 }
